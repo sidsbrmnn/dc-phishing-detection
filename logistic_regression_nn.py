2057de85db441db8b39c7f4e8293e54499648cd7
Siddharth Subramanian <sidsbrmnn@gmail.com>
2019-05-10 22:55:46 +0530

    Neural network added

--- logistic_regression_nn.py
+++ logistic_regression_nn.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from tensorflow import keras
-import numpy 
+
+import numpy
 
 from decision_tree import load_data
 
@@ -12,27 +13,23 @@ def main():
     print("Training data loaded.")
 
     model = tf.keras.models.Sequential([
-    
-      tf.keras.layers.Dense(31, activation=tf.nn.relu),
-      tf.keras.layers.Dense(150, activation=tf.nn.relu),
-      tf.keras.layers.Dropout(0.2),
-      tf.keras.layers.Dense(1, activation=tf.nn.sigmoid)
+        tf.keras.layers.Dense(31, activation=tf.nn.relu),
+        tf.keras.layers.Dense(150, activation=tf.nn.relu),
+        tf.keras.layers.Dense(100, activation=tf.nn.relu),
+        tf.keras.layers.Dense(1, activation=tf.nn.sigmoid)
     ])
-    
-    print("NN model created.")
+
+    print("Neural network model created.")
     model.compile(optimizer='adam',
-                 loss='binary_crossentropy',
-                metrics=['accuracy'])
+                  loss='binary_crossentropy',
+                  metrics=['accuracy'])
     print("Beginning model training.")
-    
-    model.fit(train_inputs, train_outputs,epochs=5)
+
+    model.fit(train_inputs, train_outputs, epochs=30)
     print("Model training completed.")
 
-    predictions = model.predict(test_inputs)
-    print("Predictions on testing data computed.")
-
     scores = model.evaluate(test_inputs, test_outputs)
-    accuracy =  scores[1]*100
+    accuracy = scores[1]*100
     print("Accuracy of logistic regression is:", accuracy)
 
 
